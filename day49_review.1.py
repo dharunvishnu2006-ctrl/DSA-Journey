f9eb86e42f6cf9014b504abad882147c4b7378dc
dharunvishnu <dharunvishnu2006@gmail.com>
2026-06-22 13:48:53 +0530

    Day 36 Review: Selection Sort + Insertion Sort

---
+++ day49_review.1.py
@@ -0,0 +1,13 @@
+arr = [2, 1, 3, 4, 5]
+
+for i in range(1, len(arr)):
+    key = arr[i]
+    j = i - 1
+
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j -= 1
+
+    arr[j + 1] = key
+
+    print(f"Pass {i}: {arr}")
