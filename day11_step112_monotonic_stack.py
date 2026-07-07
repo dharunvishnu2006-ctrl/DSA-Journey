7748818b040610c426de551c80973d37dd8606e1
dharunvishnu <dharunvishnu2006@gmail.com>
2026-07-07 12:07:06 +0530

    Day 11 Step 112: Monotonic Stack

---
+++ day11_step112_monotonic_stack.py
@@ -0,0 +1,11 @@
+def next_greater_element(arr):
+    n = len(arr)
+    result = [-1] * n        
+    stack = []                
+    for i in range(n):
+        while stack and arr[i] > arr[stack[-1]]:
+            idx = stack.pop()
+            result[idx] = arr[i]    
+        stack.append(i)
+
+    return result
