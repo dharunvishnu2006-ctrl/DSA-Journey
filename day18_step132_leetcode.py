fe536972d92943699bc8b5dd8c61b02f31447864
dharunvishnu <dharunvishnu2006@gmail.com>
2026-07-15 07:43:10 +0530

    Day 18 Step 132: Jump Game - Greedy

---
+++ day18_step132_leetcode.py
@@ -0,0 +1,11 @@
+def can_jump(nums):
+    max_reach = 0  
+
+    for i in range(len(nums)):
+        if i > max_reach:
+            return False,max_reach
+        max_reach = max(max_reach, i + nums[i])
+    return True
+
+print(can_jump([2, 3, 1, 1, 4])) 
+print(can_jump([3, 2, 1, 0, 4]))
